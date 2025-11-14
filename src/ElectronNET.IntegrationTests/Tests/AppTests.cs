ecf960c09931f31f079be9b2c3cd2f6d977f0c29
softworkz <softworkz@hotmail.com>
2025-11-14 11:45:54 +0100

    ElectronNET.IntegrationTests: Add platform support attributes

--- src/ElectronNET.IntegrationTests/Tests/AppTests.cs
+++ src/ElectronNET.IntegrationTests/Tests/AppTests.cs
@@ -1,10 +1,10 @@
 namespace ElectronNET.IntegrationTests.Tests
 {
-    using System.Runtime.InteropServices;
     using ElectronNET.API;
     using ElectronNET.API.Entities;
     using System;
     using System.IO;
+    using System.Runtime.Versioning;
     using System.Threading.Tasks;
 
     [Collection("ElectronCollection")]
@@ -61,7 +61,9 @@ namespace ElectronNET.IntegrationTests.Tests
             status.Should().NotBeNull();
         }
 
-        [Fact(Timeout = 20000)]
+        [SkippableFact(Timeout = 20000)]
+        [SupportedOSPlatform("macOS")]
+        [SupportedOSPlatform("Windows")]
         public async Task Can_get_login_item_settings()
         {
             var settings = await Electron.App.GetLoginItemSettingsAsync();
@@ -77,7 +79,9 @@ namespace ElectronNET.IntegrationTests.Tests
             (await Electron.App.CommandLine.GetSwitchValueAsync(switchName)).Should().Be("value123");
         }
 
-        [Fact(Timeout = 20000)]
+        [SkippableFact(Timeout = 20000)]
+        [SupportedOSPlatform("macOS")]
+        [SupportedOSPlatform("Windows")]
         public async Task Accessibility_support_toggle()
         {
             Electron.App.SetAccessibilitySupportEnabled(true);
@@ -96,7 +100,9 @@ namespace ElectronNET.IntegrationTests.Tests
             Electron.App.UserAgentFallback = original; // restore
         }
 
-        [Fact(Timeout = 20000)]
+        [SkippableFact(Timeout = 20000)]
+        [SupportedOSPlatform("Linux")]
+        [SupportedOSPlatform("macOS")]
         public async Task BadgeCount_set_and_reset_where_supported()
         {
             await Electron.App.SetBadgeCountAsync(2);
